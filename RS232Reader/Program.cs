bc0f311ce4f559848a790fa92316ae7fa0b50408
stytle704726 <stytle704726@gmail.com>
2023-07-24 08:47:53 +0800

    no message

---
+++ RS232Reader/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Ports;
+
+class Program
+{
+    static SerialPort serialPort;
+
+    static void Main(string[] args)
+    {
+        serialPort = new SerialPort("COM6", 9600);
+        serialPort.DataReceived += SerialPortDataReceived;
+
+        // 開啟串口
+        serialPort.Open();        
+
+        //Console.WriteLine("Press any key to send data.");
+        //Console.ReadKey();
+
+        //// 傳送資料
+        //string dataToSend = "Hello, World!";
+        //SendData(dataToSend);
+
+        //Console.WriteLine("Data sent. Press any key to exit.");
+        //Console.ReadKey();
+
+        //// 關閉串口
+        //serialPort.Close();
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+
+        serialPort.Close();
+    }
+
+    static void SerialPortDataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        SerialPort serialPort = (SerialPort)sender;
+        int bufferSize = serialPort.BytesToRead;
+        byte[] buffer = new byte[bufferSize];
+
+        // 读取字节数据到缓冲区
+        serialPort.Read(buffer, 0, bufferSize);
+        //if (buffer[1] == 0xA1)
+        //{
+
+            // 处理接收到的字节数据
+            ProcessReceivedBytes(buffer);
+            Thread.Sleep(100);
+            SendData(buffer);
+        //}
+       
+    }
+
+    static void ProcessReceivedBytes(byte[] buffer)
+    {
+        // 在这里对接收到的字节数据进行处理
+        Console.WriteLine("Received data:");
+        foreach (byte dataByte in buffer)
+        {
+            Console.Write(dataByte.ToString("X2") + " "); // 打印每个字节的十六进制表示形式            
+        }
+        Console.WriteLine();
+    }
+
+    static void SendData(byte[] data)
+    {
+        // 傳送資料到串口
+        serialPort.Write(data, 0, data.Length);
+
+        // 在這裡添加相應的處理邏輯，以應對傳送後的操作
+        Console.WriteLine("Sent: " );
+        Console.Write(BitConverter.ToString(data));
+        Console.WriteLine();
+    }
+}
